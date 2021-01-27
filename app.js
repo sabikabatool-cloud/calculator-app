07905f1d4230f9363cd7e035a8202882e5c34d47
sabikabatool-cloud <sabikabatool5@gmail.com>
2021-01-26 17:32:06 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function setNumber(num){
+var result = document.getElementById("result");
+ result.value += num;
+}
+
+function clearResult(){
+    var result = document.getElementById("result");
+    result.value = ""
+}
+
+function getResult(){
+    var result = document.getElementById("result");
+    result.value = eval(result.value)
+}
